3afd7799b4db61ab07e4b4aabd570977cd285c26
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-01-23 20:10:58 +0100

    Added animations

--- webpack.config.js
+++ webpack.config.js
@@ -76,11 +76,11 @@ module.exports = {
                 ]
             },
             {
-              test: /\.(jpg|png)$/,
-              loader: 'url-loader',
-              options: {
-                limit: 250000
-              }
+                test: /\.(jpg|png)$/,
+                loader: 'file-loader',
+                options: {
+                    name: "/assets/[name].[ext]"
+                }
             }
         ]
     },
@@ -89,5 +89,5 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {},
+    externals: {}
 };
